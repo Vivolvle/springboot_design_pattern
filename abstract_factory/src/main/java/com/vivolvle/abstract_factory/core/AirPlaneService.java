e9371f932049ccb2b0771a1741a7f919e83b9896
weiliangzheng <1067310192@qq.com>
2019-08-03 19:12:58 +0800

    让方法更加的简洁

--- abstract_factory/src/main/java/com/vivolvle/abstract_factory/core/AirPlaneService.java
+++ abstract_factory/src/main/java/com/vivolvle/abstract_factory/core/AirPlaneService.java
@@ -1,9 +1,12 @@
 package com.vivolvle.abstract_factory.core;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author weilz
  * @date 2019/8/2
  */
+@Service
 public class AirPlaneService extends AbstractService {
     @Override
     public Integer getType() {
